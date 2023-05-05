3453c3efc8f78dfde6f0936ea756d2bc9326a578
Kubistha <j.bocek.87@gmail.com>
2023-05-05 15:50:38 +0200

    přidány mezery okolo v input frame + zmena fontu

--- okno.py
+++ okno.py
@@ -8,7 +8,7 @@ window.minsize(400, 500)
 window.resizable(True, True)
 
 # fonts and colors
-main_font = ("Tahoma", 14)
+main_font = ("Verdana", 12)
 main_color = "grey"
 button_color = "blue"
 window.config(bg=main_color)
@@ -23,13 +23,13 @@ buttons_frame.pack()
 
 # input frame
 title_label = Label(input_frame, text="", width=20, bg=main_color, borderwidth=0, font=main_font)
-search_label = Label(input_frame, text="Hledej: ", width=20, bg=main_color, borderwidth=0, font=main_font)
+search_label = Label(input_frame, text="Hledat: ", width=20, bg=main_color, borderwidth=0, font=main_font)
 input_box = Entry(input_frame, width=20, borderwidth=3, font=main_font)
-title_label.grid(row=0, column=0)
-search_label.grid(row=1, column=0)
-input_box.grid(row=2, column=1)
 choice_list = ttk.Combobox(input_frame, width=20, font=main_font)
-choice_list.grid(row=2, column=0)
+title_label.grid(row=0, column=0, padx=5, pady=5)
+search_label.grid(row=1, column=0, padx=5, pady=5)
+input_box.grid(row=2, column=1, padx=5, pady=5)
+choice_list.grid(row=2, column=0, padx=5, pady=5)
 
 # text frame
 login_label = Label(text_frame, text="Login: ", width=10, bg=main_color, font=main_font)
@@ -50,7 +50,10 @@ note_label.grid(row=2, column=0)
 note_text.grid(row=2, column=1)
 
 # button frame
-edit_button = Button(buttons_frame, text="Editovat", width=10, font=main_font)
-edit_button.pack()
+edit_button = Button(buttons_frame, text="Editovat", width=10, font=main_font, bg=button_color)
+close_button = Button(buttons_frame, text="Zavřít", width=10, font=main_font , bg=button_color, command=window.destroy)
+edit_button.grid(row=0, column=0)
+close_button.grid(row=0, column=1)
+
 
 window.mainloop()
