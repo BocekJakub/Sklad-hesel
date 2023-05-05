e426b8f2a410678dd776b3d03d663aed260070b0
Kubistha <j.bocek.87@gmail.com>
2023-05-05 15:20:39 +0200

    kopie z pc

--- okno.py
+++ okno.py
@@ -22,10 +22,14 @@ text_frame.pack()
 buttons_frame.pack()
 
 # input frame
+title_label = Label(input_frame, text="", width=20, bg=main_color, borderwidth=0, font=main_font)
+search_label = Label(input_frame, text="Hledej: ", width=20, bg=main_color, borderwidth=0, font=main_font)
 input_box = Entry(input_frame, width=20, borderwidth=3, font=main_font)
-input_box.grid(row=0, column=1)
+title_label.grid(row=0, column=0)
+search_label.grid(row=1, column=0)
+input_box.grid(row=2, column=1)
 choice_list = ttk.Combobox(input_frame, width=20, font=main_font)
-choice_list.grid(row=0, column=0)
+choice_list.grid(row=2, column=0)
 
 # text frame
 login_label = Label(text_frame, text="Login: ", width=10, bg=main_color, font=main_font)
@@ -45,5 +49,8 @@ note_text = Listbox(text_frame,height=4, width=30, font=main_font)
 note_label.grid(row=2, column=0)
 note_text.grid(row=2, column=1)
 
+# button frame
+edit_button = Button(buttons_frame, text="Editovat", width=10, font=main_font)
+edit_button.pack()
 
 window.mainloop()
