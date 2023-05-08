b48f68fb594ed6d845ef1e31c5d7f1caf6d63c41
BocekJakub <118695491+BocekJakub@users.noreply.github.com>
2023-05-08 22:38:18 +0200

    Update func.py

--- func.py
+++ func.py
@@ -1,17 +1,17 @@
 import json
-from typing import List
+from tkinter import messagebox
 
 
 # function import datas (načtení databáze)
-def import_db(data):
-    with open(data, 'r') as file:
+def import_db():
+    with open('data.json', 'r') as file:
         return json.load(file)
 
 
 # function save datas (uložení nové databáze)
-def save_db(database):
+def save_db(data):
     with open('data.json', 'w') as file:
-        json.dump(database, file)
+        json.dump(data, file)
 
 
 # create list (vytvoření listu podle klíče např. list názvů, list loginu atd.)
@@ -37,12 +37,14 @@ def choice_password(data, names):
     global login
     global password
     global note
+    global alias
     for i in range(0, len(data)):
         if data[i]['name'] == names:
             login = data[i]['name']
             password = data[i]['password']
             note = data[i]['note']
-    return [login, password, note]
+            alias = data[i]['alias']
+    return [login, password, note, alias]
 
 
 # find alias
@@ -50,28 +52,49 @@ def find_alias(new, data, key):
     new = list_cycle(filter_list(data, key), 'name')
 
 # add new datas to database
-def add_new_password(data, name, password, alias, note):
-    new_data = {}
-    new_data["alias"]: List[str] = []
-    new_data["name"] = name
-    new_data["password"] = password
-    new_data["alias"] = alias
-    new_data["note"] = note
-    data.append(new_data)
+def add_new_password(data, name, password, alias, note, win):
+    new_password = {
+        "alias": alias.strip("\n"),
+        "name": name.strip("\n"),
+        "password": password.strip("\n"),
+        "note": note.strip("\n")
+    }
+    data.append(new_password)
     save_db(data)
+    import_db()
+    messagebox.showinfo("Úspěch", "Údaje byly uloženy!")
+    win.destroy()
+
+# update data (úprava údaju u hesla)
+def update_data(selected_item, login, password, note, alias, win):
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    for item in data:
+        if item['name'] == selected_item[0]:
+            item['login'] = login
+            item['password'] = password
+            item['note'] = note
+            item['alias'] = alias
+            break
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    messagebox.showinfo("Úspěch", "Údaje byly uloženy!")
+    win.destroy()
 
 
-# update password (úprava údaju u hesla)
-# zatím není naprogramováno
-def add_new_password(data, name, password, alias, note):
-    new_data = {}
-    new_data["alias"]: List[str] = []
-    new_data["name"] = name
-    new_data["password"] = password
-    new_data["alias"] = alias
-    new_data["note"] = note
-    data.append(new_data)
-    save_db(data)
+# delete data (vymazání údaju u hesla)
+def delete_data(selected_item, win):
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    for item in data:
+        if item['name'] == selected_item[0]:
+            data.remove(item)
+            break
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+    messagebox.showinfo("Úspěch", "Údaje byly vymazány!")
+    win.destroy()
+
 
 # function search button
 def action_with_arg(choice_list, data, input_box):
@@ -81,9 +104,6 @@ def action_with_arg(choice_list, data, input_box):
 def callback(self, choice_list):
     self.promena = choice_list.get()
     print(self.promena)
-#test = import_db('data.json')
-#enter = input("Hledej: ")
-#new = list_cycle(filter_list(test, enter), 'name')
-#print(new)
-#enter2 = int(input("výběr hesla: "))
-#choice_password(test, new, enter2)
+
+
+
